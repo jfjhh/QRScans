48c87f18d92a531341274b17eff09096bf6a5b4d
Alex Striff <alex.striff1@gmail.com>
2016-05-01 14:53:16 -0700

    Thumbnails

--- qr-sync.sh
+++ qr-sync.sh
@@ -6,8 +6,9 @@
 
 [ -d "$1" ] || return 1
 DIR="$1"
+INDEX="$DIR/index.html"
 
-IMAGES=(`ls $DIR/ | grep ".jpg$" | sort -n`)
+IMAGES=(`ls $DIR/ | grep "[0-9].jpg$" | sort -n`)
 
 FIRST=`basename ${IMAGES[0]} | sed 's/\.jpg$/\.html/'`
 LAST=`basename ${IMAGES[$((${#IMAGES[@]} - 1))]} | sed 's/\.jpg$/\.html/'`
@@ -17,8 +18,11 @@ for ((i = 0; i < ${#IMAGES[@]}; i++)); do
 	n="`basename -s '.jpg' ${img}`"
 	info="$(cd $DIR && file -b ${IMAGES[$i]} | sed 's%,%\n%g')"
 	h="${n}.html"
+	t="${n}_t.jpg"
 
 	echo $i ${IMAGES[$i]} >&2
+
+	! [ -e "${DIR}/${t}" ] && convert $img -resize 10% "${DIR}/${t}"
 
 	[ $i -gt 0 ] \
 		&& p="`basename ${IMAGES[$(($i - 1))]} | sed 's/\.jpg$/\.html/'`" || p=""
@@ -45,8 +49,8 @@ for ((i = 0; i < ${#IMAGES[@]}; i++)); do
 	<body>
 		<a class="image" href="/s/${n}.jpg">
 			<h1>Scan ${n}</h1>
-			<img src="/s/${n}.jpg" alt="Scanned Image $n"
-			title="Updated: `date -I`" height="100%" width="100%"/>
+			<img src="/s/${t}" alt="Scanned Image $n"
+			title="Updated: `date -I`" />
 			<h3>Click to Enlarge.</h3>
 			<h2>
 				<a href="/s/${FIRST}">&lt;&lt;</a>
@@ -62,6 +66,48 @@ for ((i = 0; i < ${#IMAGES[@]}; i++)); do
 </html>
 	EOF
 done
+
+cat <<-EOF > $INDEX
+<!DOCTYPE html>
+
+<!--
+   __     ______     __     __  __     __  __
+  /\\ \\   /\\  ___\\   /\\ \\   /\\ \\_\\ \\   /\\ \\_\\ \\
+ _\\_\\ \\  \\ \\  __\\  _\\_\\ \\  \\ \\  __ \\  \\ \\  __ \\
+/\\_____\\  \\ \\_\\   /\\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\
+\\/_____/   \\/_/   \\/_____/   \\/_/\\/_/   \\/_/\\/_/
+-->
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Site J.2 - Scan Index</title>
+		<link rel="stylesheet" type="text/css" href="/css/scan.css" />
+	</head>
+	<body>
+EOF
+
+for ((i = 0; i < ${#IMAGES[@]}; i++)); do
+	img="${DIR}/${IMAGES[$i]}"
+	n="`basename -s '.jpg' ${img}`"
+	h="${n}.html"
+	t="${n}_t.jpg"
+	cat <<-EOF >> $INDEX
+<!-- Thumbnail for image ${n}. -->
+<div class="thumb">
+	<a class="image" href="/s/${n}.jpg">
+		<img src="/s/${t}" alt="Scanned Image $n"
+		title="Updated: `date -I`" />
+	</a>
+</div>
+
+	EOF
+done
+
+cat <<-EOF >> $INDEX
+	</body>
+</html>
+EOF
 
 rsync -av --delete --checksum ${DIR}/ jfjhh@nouveau.local:/var/www/scans/
 
